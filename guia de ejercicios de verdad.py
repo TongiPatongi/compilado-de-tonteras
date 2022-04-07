27833a794fca6b65f58397e8d31e3d64aa7a5f85
TongiPatongi <103199163+TongiPatongi@users.noreply.github.com>
2022-04-07 11:13:34 -0400

    Add files via upload

---
+++ guia de ejercicios de verdad.py
@@ -0,0 +1,19 @@
+#2
+def contar (string):
+    caracter=0
+    for j in string:
+        if j=="a":
+            caracter+=1
+    print("la cantidad de caractres es:",caracter)
+string=input("")
+print(contar(string))
+#parte 2
+def eliminar(string):
+    caracter=0
+    for j in range(len(caracter)):
+        if j=="a":
+            string = string.replace(caracter[a],"")
+    print(elimninar(string))
+
+
+    
