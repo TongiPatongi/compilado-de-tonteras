c22c09ebf2d51df8b632461e75745887f11440ef
TongiPatongi <103199163+TongiPatongi@users.noreply.github.com>
2022-05-12 11:11:01 -0400

    Add files via upload

---
+++ clase python archivos god no funciona.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+archivo = open("C:/Users/shart/OneDrive/Escritorio/datos.txt")
+lineas = archivo.readlines()
+for linea in lineas:
+
+    print(lineas)
+archivo.close()
